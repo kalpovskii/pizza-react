4745a5f2d27dd2891b0ef32f30efc891442b17c3
kalpovskii_a <aserenru@gmail.com>
2022-10-11 00:52:18 +0300

    Add prettier, keys and sort option

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import './scss/app.scss';
-import Header from './components/Header.jsx';
+import "./scss/app.scss";
+import Header from "./components/Header.jsx";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import Pizza_block from "./components/Pizza_block";
 
+import items from "./pizza.json";
 
 function App() {
-    return (
-        <div className="wrapper">
-            <Header/>
-            <div className="content">
-                <div className="container">
-                    <div className="content__top">
-                        <Categories />
-                        <Sort />
-                    </div>
-                    <h2 class="content__title">Все пиццы</h2>
-                    <div class="content__items">
-                        <Pizza_block />
-                        <Pizza_block />
-                        <Pizza_block />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            {items.map((obj) => (
+              <Pizza_block key={obj.id} {...obj} />
+            ))}
+          </div>
         </div>
-    )
-};
+      </div>
+    </div>
+  );
+}
 
 export default App;
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,33 +1,33 @@
-import {useState} from "react";
+import { useState } from "react";
 
 function Categories() {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const categories = [
-        'Все',
-        'Мясные',
-        'Вегетарианская',
-        'Гриль',
-        'Острые',
-        'Закрытые',
-    ];
+  const [activeIndex, setActiveIndex] = useState(0);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
 
-    const onClickCategory = (index) => {
-        setActiveIndex(index)
-    };
-
-    return (
-        <div className="categories">
-            <ul>
-                {
-                    categories.map((value, index) => (
-                        <li onClick={() => {
-                            onClickCategory(index)
-                        }} className={activeIndex === index ? "active" : ""}>{value}</li>
-                    ))
-                }
-            </ul>
-        </div>
-    );
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li
+            key={index}
+            onClick={() => {
+              setActiveIndex(index);
+            }}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Categories;
