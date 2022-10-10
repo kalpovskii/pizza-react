21397d63319e6c49b1432df0829269edb2b07a73
kalpovskii_a <aserenru@gmail.com>
2022-10-10 19:11:05 +0300

    Add html and jsx components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import Header from './components/Header.jsx';
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import Pizza_block from "./components/Pizza_block";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 class="content__title">Все пиццы</h2>
+                    <div class="content__items">
+                        <Pizza_block />
+                        <Pizza_block />
+                        <Pizza_block />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
 
 export default App;
